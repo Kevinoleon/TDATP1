f98c8541bb45f32195e39cacdae2520379674cd5
gatti2602 <gatti2602@gmail.com>
2017-04-09 21:28:42 -0300

    Clase Grafo dirigido

---
+++ src/com/fiuba/grafos/Digrafo.java
@@ -0,0 +1,74 @@
+package com.fiuba.grafos;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by gatti2602 on 09/04/17.
+ * Implementa la clase Digrafo inmutable.
+ * Representa los vertices como una entrada en la lista de adyacencia
+ * Cada vertice se representa por una lista de Aristas. Los vertices empiezan en 0
+ * El digrafo es no pesado.
+ */
+public class Digrafo {
+
+    private ArrayList<ArrayList<Arista>> adjList;
+
+    Digrafo(Integer vertices) {
+        adjList = new ArrayList<>();
+        for (int i = 0; i < adjList.size(); i++)
+            adjList.add(new ArrayList<>());
+    }
+
+    /**
+     * Devuelve la cantidad de Vertices del Grafo
+     */
+    public Integer cuentaDeVertices() {
+        return adjList.size();
+    }
+
+    /**
+     * Devuelve la cantidad de Aristas del Grafoo
+     */
+    public Integer cuentaDeAristas() {
+        Integer aux = 0;
+        for (ArrayList lista : adjList) {
+            aux += lista.size();
+        }
+        return aux;
+    }
+
+    /**
+     * Devuelve un iterador sobre la lista de adyacencia del vertice indicado.
+     * Devuelve null si el vertice no existe
+     */
+    public Iterator<Arista> getAdjList(Integer vertice) {
+        if (vertice >= adjList.size())
+            return null;
+        return adjList.get(vertice).iterator();
+    }
+
+    /**
+     * Agrega una Arista si el src esta dentro de los vertices del grafo y la arista no existe
+     */
+    public void agregarArista(Integer src, Integer dst) {
+
+        if (src < adjList.size()) {
+            ArrayList<Arista> verticeAdjList = adjList.get(src);
+            Boolean existe = false;
+
+            //Chequeo que no exista la arista
+            Iterator<Arista> it = verticeAdjList.iterator();
+            while (it.hasNext() && !existe) {
+                Arista e = it.next();
+                if (e.getDst().equals(dst))
+                    existe = true;
+            }
+            if (!existe)
+                verticeAdjList.add(new Arista(src, dst));
+        }
+
+    }
+
+
+}
