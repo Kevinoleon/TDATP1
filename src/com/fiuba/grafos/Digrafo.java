ea173a3d1b141e24d8b9d3076350906546656d34
gatti2602 <gatti2602@gmail.com>
2017-04-16 11:54:38 -0300

    DFS y transponer Grafo

--- src/com/fiuba/grafos/Digrafo.java
+++ src/com/fiuba/grafos/Digrafo.java
@@ -12,6 +12,10 @@ import java.util.Iterator;
  */
 public class Digrafo {
 
+    /**
+     * Cada elemento contiene la lista de arsitas del vertice
+     * referenciado
+     */
     private ArrayList<ArrayList<Arista>> adjList;
 
     Digrafo(Integer vertices) {
@@ -79,4 +83,52 @@ public class Digrafo {
         return existe;
     }
 
+    /**
+     * Devuelve una lista con los vertices visitados desde el src
+     *
+     * @param src
+     * @return
+     */
+    public ArrayList<Integer> DFS(Integer src) {
+
+        if (src >= this.cuentaDeVertices())
+            return null;
+
+        ArrayList<Integer> listaVerticesVisitados = new ArrayList<>();
+        Boolean[] verticeVisitado = new Boolean[this.cuentaDeVertices()];
+
+        //Hago DFS desde el vertice indicado
+        DFS_Visitar(src, listaVerticesVisitados, verticeVisitado);
+
+        return listaVerticesVisitados;
+
+
+    }
+
+    private void DFS_Visitar(Integer v, ArrayList<Integer> listaVerticesVisitados, Boolean[] verticeVisitado) {
+
+        verticeVisitado[v] = true;
+        listaVerticesVisitados.add(v);
+
+        //El orden de visita depende de este iterador
+        Iterator<Arista> it = this.getAdjList(v);
+        while (it.hasNext()) {
+            Arista a = it.next();
+            if (!verticeVisitado[a.getDst()]) {
+                DFS_Visitar(v, listaVerticesVisitados, verticeVisitado);
+            }
+        }
+    }
+
+    public Digrafo transponer() {
+        Digrafo d = new Digrafo(this.cuentaDeVertices());
+
+        for (int src = 0; src < this.cuentaDeVertices(); src++) {
+            for (int dst = 0; dst < this.cuentaDeVertices(); dst++) {
+                if (!existeArista(src, dst))
+                    d.agregarArista(src, dst);
+            }
+        }
+        return d;
+    }
 }
